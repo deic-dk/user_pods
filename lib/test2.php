e8c4d3e7976a685aa869e8d07fa552ef91327832
Charlie Root <root@silo7.sciencedata.dk>
2021-03-04 11:39:35 +0100

    Small clean-up

--- lib/test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = 'https://10.0.0.12/get_containers.php?user_id=fror@dtu.dk';
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HEADER, false);
-$data = curl_exec($curl);
-curl_close($curl);
-print_r($data);
