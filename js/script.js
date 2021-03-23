bf9c8aa5ecb29474ee486a71514719d73c3349ac
marpapdtu <marpap@dtu.dk>
2021-03-23 20:40:34 +0100

    box,headline + httpsport

--- js/script.js
+++ js/script.js
@@ -38,9 +38,9 @@ $(document).ready(function() {
 //	});
 
 
-	$("#podstable .nametext").live('click', function(){
+	$("#podstable .name").live('click', function(){
               var pod = $(this).closest('td').attr('id');
-	      var httpsport = $("td[class='"+httpsport+"']").find("span#httpsport").html();
+	      var httpsport = $(this).closest('tr').find("span#https-port").html();
 	      var Uri_Jupy = $(this).closest('tr').find('span#Uri_Jupy').html(); 
 	      var Uri_Jupy_all = 'https://kube.sciencedata.dk:' + httpsport + '/' + Uri_Jupy;
 	      var html = '<div><span><h3 class="oc-dialog-title" style="padding-left:25px;">More info for <span>'+ pod+'</span></h3></span><a class="oc-dialog-close close svg"></a>\
@@ -48,7 +48,7 @@ $(document).ready(function() {
 		          <div style="position:absolute; top:80px; left:40px;">\
 		          <div id="Uri_Jupy">Direct Link:</div><p></p>\
 		          <div><a href=\''+Uri_Jupy_all+'\'target="_blank">'+ Uri_Jupy_all +'</a></div></div>\
-                          <div style="position:absolute; bottom:100px; left:60px;">\
+			  <div style="position:absolute; bottom:100px; left:60px;">\
 			  <div>Download the logs of the container:</div><p></p>\
 			  <button id="download_logs" class="download btn btn-primary btn-flat">Download</button>&nbsp\
                           </div>\
