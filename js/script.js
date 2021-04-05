ea7fa1deee01a9b86eb4f2bd52e0b699855b3977
Frederik Orellana <frederik@orellana.dk>
2021-04-05 17:24:13 +0200

    Updated from kubernetes_app branch (Ioanna)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	var hostname = $(location).attr('host');;
+
+	var dockerhub_uri = 'https://hub.docker.com/r/';
+	var github_uri = 'https://github.com/deic-dk/pod_manifests/blob/main/';
+
 	$('a#pod-create').click(function() {
 		$('#newpod').slideToggle();
 	});
@@ -7,12 +12,52 @@ $(document).ready(function() {
 		$('#newpod').slideToggle();
 	});
 
+	$("#podinput").prop("selectedIndex", -1);
+
+	$("#podinput").change(function () {
+        	var select_value = $(this).val()
+		$.post(OC.filePath('kubernetes_app', 'ajax', 'actions.php') ,{ yaml_file : select_value } ,  function (jsondata){
+                            if(jsondata.status == 'success'){
+				var image_github_uri = github_uri + select_value;
+				var image_dockerhub_uri = dockerhub_uri + jsondata.data.included[2];
+				var dockerhub_description = jsondata.data.included[3];
+
+				var webdav_link = 'https://' + hostname + '/storage/' + OC.currentUser;
+				var webdav_link_ref  = '<a href=\''+webdav_link+'\'target="_blank">'+webdav_link+'</a>';
+				var image_info = '<span style="padding-left:1%"><a href=\''+image_github_uri+'\'target="_blank">GitHub page</a></span>\
+				    			<span style="padding-left:1%"><a href=\''+image_dockerhub_uri+'\'target="_blank">DockerHub page</a></span>';
+				$('#links').empty();
+				$('#links').append(image_info);
+
+				$('#description').empty();
+				$('#description').append(dockerhub_description);
+
+				if (jsondata.data.included[0]==true) {
+					$('div#ssh').css('visibility', 'visible');
+				}
+				else {
+					$('div#ssh').css('visibility', 'hidden');
+				}
+				if (jsondata.data.included[1]==true) {
+					$('div#storage').css('visibility', 'visible');
+					$('#webdav').empty();
+					$('#webdav').append(webdav_link_ref);
+
+				}
+				else {
+					$('div#storage').css('visibility', 'hidden');
+				}
+			    }
+
+		});
+    	});
+
 	$('#newpod #ok').on('click', function() {
 		var yaml_file = $('#podinput').val();
 		var ssh_key = $('.sshpod').val();
 		var storage = $('.storagepath').val();
 		if( ssh_key != "" && storage != "") {
-			$.ajax({url: OC.filePath('kubernetes_app', 'ajax', 'actions.php'),
+			$.ajax({url: OC.filePath('user_pods', 'ajax', 'actions.php'),
 				data: {pod_image: yaml_file, ssh: ssh_key, storage: storage}, 
 				method: 'post',
 				beforeSend: function() {
@@ -28,15 +73,14 @@ $(document).ready(function() {
     				success: function(data) {
 					location.reload();
 
-  				}				
-			});
-		}
+			}				
+		});
 	});
 
 	$("#podstable td #delete-pod").live('click', function() {
 		var podSelected = $(this).closest('tr').attr('id');
                 $( '#dialogalert' ).dialog({ buttons: [ { id:'test','data-test':'data test', text: 'Delete', click: function() {
-			$.post(OC.filePath('kubernetes_app', 'ajax', 'actions.php') ,{ pod_name : podSelected } ,  function (jsondata){ 
+			$.post(OC.filePath('user_pods', 'ajax', 'actions.php') ,{ pod_name : podSelected } ,  function (jsondata){ 
 
                             if(jsondata.status == 'success'){ 
                              	location.reload();
@@ -46,7 +90,7 @@ $(document).ready(function() {
 
 		      });
 			
-			$.ajax({url: OC.filePath('kubernetes_app', 'ajax', 'actions.php'),
+			$.ajax({url: OC.filePath('user_pods', 'ajax', 'actions.php'),
 				data: {pod_name: podSelected},
 				method: 'post',
 				beforeSend: function() {
@@ -86,12 +130,16 @@ $(document).ready(function() {
 		var uri = $(this).closest('tr').find('span#uri').html();
 		var complete_uri = 'https://kube.sciencedata.dk:' + https_port + '/' + uri;
 
+		var image = $(this).closest('tr').find('span#image').html();
+		var image_uri = dockerhub_uri + image;
 		var html = '<div><span><h3 class="oc-dialog-title" style="padding-left:25px;"><span>'+ pod+'</span></h3></span><a class="oc-dialog-close close svg"></a>\
 			<div id="meta_data_container" class=\''+ pod +'\'>\
-			<div style="position:absolute; left:40px; top:100px;">\
-			<div id="uri">Access web service:</div><p></p>\
+			<div style="position:absolute; left:40px; top:80px;">Original image on Docker Hub:\
+		        <div><a href=\''+image_uri+'\'target="_blank">'+ image_uri + '</a></div></div>\
+			<div style="position:absolute; left:40px; top:140px;">\
+			<div id="uri">Access web service:</div>\
 			<div><a href=\''+complete_uri+'\'target="_blank">'+ complete_uri +'</a></div></div>\
-          		<div style="position:absolute; bottom:100px; left:40px;" >\
+          		<div style="position:absolute; bottom:40px; left:40px;" >\
 			<div>Download the logs of your container:</div><p></p>\
 			<button id="download-logs" class="download btn btn-primary btn-flat">Download</button>&nbsp\
 			</div>\
@@ -102,7 +150,7 @@ $(document).ready(function() {
 			  resizeable: false,
 			  draggable: false,
 			  height: 400,
-			  width: 400
+			  width: 600
 			});
 
 		$('body').append('<div class="modalOverlay">');
@@ -115,7 +163,7 @@ $(document).ready(function() {
 		$('.ui-helper-clearfix').css("display", "none");
 
 		$("#download-logs").live('click', function() {
-			OC.redirect(OC.linkTo('kubernetes_app', 'ajax/getLogs.php') + '?file=' + pod);
+			OC.redirect(OC.linkTo('user_pods', 'ajax/getLogs.php') + '?file=' + pod);
 		});
 	});
 
--- lib/util.php
+++ lib/util.php
@@ -1,88 +1,98 @@
 <?php
 
 class OC_Kubernetes_Util {
-	private static $KUBE_URL = 'http://10.0.0.12/';
+    private static $CADDY_URI = 'http://10.0.0.12/';
+    private static $GITHUB_URI = 'https://raw.githubusercontent.com/deic-dk/pod_manifests/main/';
+    private static $DOCKERHUB_URI = 'https://hub.docker.com/v2/repositories/';
+     /**
+     * @brief Get all user's pods
+     * @param  string $uid Name of the user
+     * @return array  with pod names of a user
+     */
 
-	public static function getUserPods($uid){
-	$table = array();
-	// First get fields
-	// pod_name|container_name|image_name|pod_ip|node_ip|owner|age(s)|status|ssh_port|https_port|uri
-	$url = OC_Kubernetes_Util::$KUBE_URL."get_containers.php?fields=true&user_id=".$uid;
-	$response = file_get_contents($url);
-	$fields = explode("|", $response);
-	// Now the values
-	$url = OC_Kubernetes_Util::$KUBE_URL."get_containers.php?user_id=".$uid;
-	$response = file_get_contents($url);
-	$rows = explode("\n", $response);	
+    public static function createStorageDir($uid)
+    {
+	  $folder_path = "/tank/storage/".$uid;
+
+	  if (!is_dir($folder_path))
+	  {
+    		mkdir($folder_path, 0755, true);
+	  } 
+    }
+
+    public static function getUserPods( $uid )
+    {
+  	$table = array();		
+	$complete_uri = OC_Kubernetes_Util::$CADDY_URI."get_containers.php?user_id=".$uid;
+	$response = file_get_contents($complete_uri);
+	$rows = explode("\n", $response);
+	array_pop($rows);
 	foreach ($rows as $row) {
-		if(empty($row)){
-			continue;
-		}
-		$values = explode("|", $row);
-		$container = [];
-		$i = 0;
-		foreach($values as $value){
-			$container[$fields[$i++]] = $value??"";
-		}
+		$container = array("pod_name"=>"","container_name"=>"", "image"=>"", "status"=>"", "ssh_port"=>"", "https_port"=> "", "uri"=>"");
+		$cells = explode("|", $row);
+
+		$container["pod_name"] = $cells[0] ?? "";
+		$container["container_name"] = $cells[1] ?? "";
+		$container["image"] = $cells[2] ?? "";
+		$container["status"] = $cells[7] ?? "";
+		$container["ssh_port"] = $cells[8] ?? "";
+		$container["https_port"] = $cells[9] ?? "";
+		$container["uri"] = $cells[10] ?? "";
 		array_push($table, $container);
-	}
-		return $table;
-	}
+	}	
+        return $table;
+    }
 
-	public static function addRow($index, $value){
-	echo "<td id=\"".$index."\" class=\"$value\">
-				 <div class='col-xs-8 filelink-wrap'>
-						<span>".$value."</span>
-				 </div>
-				</td>";
-	}
+    public static function addRow($index, $value)
+    {
+	echo "<td id=\"$index\"  class=\"$value\">
+                 <div class=\"$index\">
+                      <span id=\"$index\">$value</span>
+                 </div>
+              </td>";
+    }
 
+    private static function getContent($uri) 
+    {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $uri);
+	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+    }
 
-	public static function getImages(){
-	$dir =	'pod_manifests';
-	$dir = \OC\Files\Filesystem::normalizePath($dir);
-
+    public static function getImages()
+    {
+	$default_pods_uri =  'https://github.com/deic-dk/pod_manifests';
 	try {
-		$dirInfo = \OC\Files\Filesystem::getFileInfo($dir);
-		if (!$dirInfo || !$dirInfo->getType() === 'dir') {
-			header("HTTP/1.0 404 Not Found");
-			exit();
-		}
-
-		$permissions = $dirInfo->getPermissions();
-
-		$sortAttribute = isset($_GET['sort']) ? $_GET['sort'] : 'name';
-		$sortDirection = isset($_GET['sortdirection']) ? ($_GET['sortdirection'] === 'desc') : false;
-
-		// make filelist
-
-		$files = \OCA\Files\Helper::getFiles($dir, $sortAttribute, $sortDirection);
-		$data = \OCA\Files\Helper::formatFileInfos($files);
-
+		$res = self::getContent($default_pods_uri);
+		$type = '.yaml';
 		$filenames = array();
-		foreach ($data as $file) {
-			array_push($filenames, $file['name']);
+
+		$dom = new DomDocument();
+		$dom->loadHTML($res, LIBXML_NOERROR);
+
+		$finder = new DomXPath($dom);
+		$classname="js-navigation-open Link--primary";
+		$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+		foreach( $nodes as $elem ) {
+    			$filename = $elem->textContent;
+			$len = strlen($type);
+
+			$is_yaml = (substr($filename, -$len) === $type);
+			if ($is_yaml == true) {
+				array_push($filenames, $filename);
+			}
 		}
+
 		return $filenames;
 
-	} catch (\OCP\Files\StorageNotAvailableException $e) {
-		\OCP\Util::logException('files', $e);
-		OCP\JSON::error(array(
-			'data' => array(
-			'exception' => '\OCP\Files\StorageNotAvailableException',
-			'message' => $l->t('Storage not available')
-			)
-		));
-	} catch (\OCP\Files\StorageInvalidException $e) {
-		\OCP\Util::logException('files', $e);
-		OCP\JSON::error(array(
-			'data' => array(
-			'exception' => '\OCP\Files\StorageInvalidException',
-			'message' => $l->t('Storage invalid')
-			)
-		));
 	} catch (\Exception $e) {
-		\OCP\Util::logException('files', $e);
+		\OCP\Util::logException('Pods', $e);
 		OCP\JSON::error(array(
 			'data' => array(
 			'exception' => '\Exception',
@@ -91,36 +101,100 @@ class OC_Kubernetes_Util {
 		));
 	}
 
+  }
+
+  public static function getDockerhubDescription($image_name)
+  {
+	  $dockerhub_uri = self::$DOCKERHUB_URI.$image_name.'/';
+	  $dict = json_decode(self::getContent($dockerhub_uri));
+
+	  $description = $dict->{'full_description'};
+	  return $description;
+  }
+
+  public static function checkImage($yaml_file)
+  {     
+	$github_uri = self::$GITHUB_URI.$yaml_file;
+
+	$yaml_content = self::getContent($github_uri);
+
+	$has_ssh = false;
+	$has_mount = false;
+
+	$temp = explode("image:",$yaml_content);
+	$image_name = trim(explode(PHP_EOL, $temp[1])[0]);
+
+	$image_description = self::getDockerhubDescription($image_name);
+
+	if( strpos($yaml_content, "SSH_PUBLIC_KEY") !== false) {
+		$has_ssh = true;	
 	}
 
+	if( strpos($yaml_content, "mountPath") != false) {
+		$has_mount = true;
+	}
+	return array($has_ssh, $has_mount, $image_name, $image_description);
+  }
 
-	public static function createPod($yaml_file, $ssh_key, $storage_path, $uid){
-	$encoded_key = rawurlencode ($ssh_key);
-	$complete_uri = OC_Kubernetes_Util::$KUBE_URL."run_pod.php?user_id=".$uid."&storage_path=".$storage_path."&public_key=".$encoded_key."&yaml_uri=/files/pod_manifests/".$yaml_file;
+  public static function createPod($yaml_file, $ssh_key, $storage_path, $uid)
+  {
+	$complete_uri = self::$CADDY_URI."run_pod.php?user_id=".$uid."&yaml_uri=/files/pod_manifests/".$yaml_file;
+	if (is_null($ssh_key) != false) {
+		$encoded_key = rawurlencode($ssh_key);
+		if (is_null($storage_path) != false) {
+			$complete_uri = $complete_uri."&storage_path=".$storage_path."&public_key=".$encoded_key;
+		} else  {
+			$complete_uri = $complete_uri."&public_key=".$encoded_key;
+		}
+	} else {
+		if (is_null($storage_path) != false) {
+			$complete_uri = $complete_uri."&storage_path=".$storage_path;
+		}
+	}
+
 	$response = file_get_contents($complete_uri);
 	// TODO Add exceptions and handling
 	return $response;
-	} 
+  } 
 
-	public static function deletePod($pod_name, $uid) {
-		$complete_uri = OC_Kubernetes_Util::$KUBE_URL."delete_pod.php?user_id=".$uid."&pod=".$pod_name;
-		$response = file_get_contents($complete_uri);
-		return $response;
+  private static function getAppDir($user){
+	\OC_User::setUserId($user);
+	\OC_Util::setupFS($user);
+	$fs = \OCP\Files::getStorage('kubernetes_app');
+	if(!$fs){
+		\OC_Log::write('kubernetes_app', "ERROR, could not access files of user ".$user, \OC_Log::ERROR);
+		return null;
 	}
+	return $fs->getLocalFile('/');
+  }
 
-	public static function getLogs($pod_name, $uid){
-		$file_path = "/tmp/";
-		$complete_uri = OC_Kubernetes_Util::$KUBE_URL."get_pod_logs.php?user_id=".$uid."&pod=".$pod_name;
-		$response = file_get_contents($complete_uri);
+  public static function deletePod($pod_name, $uid) 
+  {
+	  $complete_uri = OC_Kubernetes_Util::$CADDY_URI."delete_pod.php?user_id=".$uid."&pod=".$pod_name;
+	  $response = file_get_contents($complete_uri);
+	  return $response;
+  }
 
-		$file = $file_path.$pod_name."log";
-		$logfile = fopen($file, "w") or die("Unable to open file!");
-		fwrite($logfile, $response);
-		fclose($logfile);
+  public static function getLogs($pod_name, $uid)
+  {
+	  $file_path = self::getAppDir($uid)."/pod_logs/";
 
-		$type = filetype($file);
-		header("Content-type: $type");
-			header("Content-Disposition: attachment;filename=$pod_name.log");
-		 		readfile($file);
-	}
+	  if (!is_dir($file_path))
+	  {
+    		mkdir($file_path, 0750, true);
+	  }	
+	  
+	  $complete_uri = OC_Kubernetes_Util::$CADDY_URI."get_pod_logs.php?user_id=".$uid."&pod=".$pod_name;
+	  $response = file_get_contents($complete_uri);
+
+	  $file = $file_path.$pod_name.".log";
+	  $logfile = fopen($file, "w") or die("Unable to open file!");
+	  fwrite($logfile, $response);
+	  fclose($logfile);
+
+	  $type = filetype($file);
+	  header("Content-type: $type");
+  	  header("Content-Disposition: attachment;filename=$pod_name.log");
+       	  readfile($file);
+  }
 }
