f3c76f803a9238feb2ae9d7cbefae5b5be1b726e
marpapdtu <marpap@dtu.dk>
2021-04-16 14:49:04 +0200

    Dowload_logs

--- lib/backend.php
+++ lib/backend.php
@@ -16,31 +16,71 @@ class OC_Kubernetes {
 	$rows = explode("\n", $response);
 	array_pop($rows);
 	foreach ($rows as $row) {
-		$container = array("pod_name"=>"","container_name"=>"","status"=>"", "ssh_port"=>"", "https_port"=> "", "Uri_Jupy"=> "");
-		$cells = explode("|", $row);
+               $container = array("pod_name"=>"", "container_name"=>"", "status"=>"", "ssh_port"=>"", "https_port"=>"", "Uri_Jupy"=>"", "docker_image"=>"");
+               $cells = explode("|", $row);
 
-		$container["pod_name"] = $cells[0] ?? "";
-		$container["container_name"] = $cells[1] ?? "";
-		$container["status"] = $cells[7] ?? "";
-		$container["ssh_port"] = $cells[8] ?? "";
-		$container["https_port"] = $cells[9] ?? "";
-		$container["Uri_Jupy"]= $cells[10] ?? "";
-               #### $container["owner"]= $cells[6] ?? "";
+	       $container["pod_name"] = $cells[0] ?? "";
+	       $container["container_name"] = $cells[1] ?? "";
+	       $container["status"] = $cells[7] ?? "";
+	       $container["https_port"] = $cells[9] ?? "";
+	       $container["ssh_port"] = $cells[8] ?? "";
+	       $container["Uri_Jupy"] = $cells[10] ?? "";
+	       $container["docker_image"] = $cells[2] ?? "";
 		array_push($table, $container);
-	}	
+	}
         return $table;
     }
 
 
 
-    public static function deletePod( $pod_name, $uid ){
+    public static function deletePod( $pod, $uid ){
     
-	    $full_URI = OC_Kubernetes::$URI."delete_pod.php?user_id=".$uid."&pod=".$pod_name;
-
-
+	    $full_URI = OC_Kubernetes::$URI."delete_pod.php?user_id=".$uid."&pod=".$pod;
 	    $response = file_get_contents($full_URI);
 	    return $response;
-         }    
+    }   
     
+    public static function createPath($uid){
+        $path = "/tank/storage/" .$uid;
+	if (!is_dir($path)){
+	   mkdir($path, 0750, true);
+	}
+    
+    }
+
+    
+    public static function createUserFolder($uid){
+	 $fs = \OCP\Files::getStorage('kubernetes');
+	    if (!$fs){
+	       \OCP\Util::writeLog('kubernetes', 'Could not create a folder' .$uid, \OCP\Util::ERROR);
+	        return false;
+                }
+          $dir = \OC\Files\Filesystem::normalizePath('/'.$uid);
+          return $fs->getLocalFile($dir);
+    }
+
+
+      public static function getLogs( $pod, $uid ){
+
+          $log = self::createUserFolder($uid) .  "/logs";
+	     if (!file_exists($log)){
+	     
+	        mkdir($log,0750, true);
+	       }  
+
+	    $uri_log = OC_Kubernetes::$URI . "get_pod_logs.php?user_id=" . $uid . "&pod=" . $pod;
+	    $response = file_get_contents($uri_log);
+	    $log_file = $log . $pod . ".log"; 
+            $final_file = file_put_contents($log_file, $response);
+	    return $log_file;
+      }
+
+/* public static function createPod($yaml_uri, $uid, $ssh_key, $storage_path){
+       $full_uri = self::$URI . "run_pod.php?user_id=" . $uid . "&storage_path=" . $storage_path ."&public_key=" . $ssh_key
+    
+    
+}*/
 
 }
+
+
