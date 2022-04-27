7ee35d6198c9a4fa4044d7405774aadd724f3abc
joshuadclement <joshua.clement@i2.dk>
2022-04-27 17:28:02 +0200

    Allow potentially long text to wrap in status and expanded rows

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ function getRowElementPlain(name, value) {
 
 function getRowElementView(name, container) {
 	if (~container['status'].indexOf("Running")) {
-		return "\n <td>\n  <div column='" + name + "'>\n   <span><a href='" + container['url'] +
-			"'>link</a></span>\n  </div>\n </td>";
+		if (container['url'].length) {
+			return "\n <td>\n  <div column='" + name + "'>\n   <span><a href='" + container['url'] +
+				"'>link</a></span>\n  </div>\n </td>";
+		} else {
+			return getRowElementPlain(name, "none");
+		}
 	}
 	return getRowElementPlain(name, "wait");
 }
@@ -14,13 +18,13 @@ function getRowElementView(name, container) {
 function getExpandedTable(container) {
 	var str = "\n <tr hidden class='expanded-row' pod_name='" + container['pod_name'] + "'> <td colspan='5'>" +
 		"\n<table id='expanded-" + container['pod_name'] + "' class='panel expanded-table'>" +
-		"\n <tr><td class='expanded-column-name'>container name:</td> <td>" + container['container_name'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>image name:</td> <td>" + container['image_name'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>pod IP:</td> <td>" + container['pod_ip'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>node IP:</td> <td>" + container['node_ip'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>owner:</td> <td>" + container['owner'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>age:</td> <td>" + container['age'] + "</td class='expanded-column-value'></tr>" +
-		"\n <tr><td class='expanded-column-name'>ssh url:</td> <td>" + (container['ssh_url'].length === 0 ? "none" : "<a href='" + container['ssh_url'] + "'>copy</a>") + "</td class='expanded-column-value'></tr>" +
+		"\n <tr><td class='expanded-column-name'>container name:</td> <td class='expanded-column-value'><span>" + container['container_name'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>image name:</td> <td class='expanded-column-value'><span>" + container['image_name'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>pod IP:</td> <td class='expanded-column-value'><span>" + container['pod_ip'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>node IP:</td> <td class='expanded-column-value'><span>" + container['node_ip'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>owner:</td> <td class='expanded-column-value'><span>" + container['owner'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>age:</td> <td class='expanded-column-value'><span>" + container['age'] + "</span></td></tr>" +
+		"\n <tr><td class='expanded-column-name'>ssh url:</td> <td class='expanded-column-value'><span>" + (container['ssh_url'].length === 0 ? "none" : "<a href='" + container['ssh_url'] + "'>copy</a>") + "</span></td></tr>" +
 		"\n</table>" +
 		"\n </td> </tr>";
 	return str;
@@ -163,10 +167,6 @@ function deletePod(podName) {
 			$('#podstable tr[pod_name="' + podName + '"] td a.delete-pod').hide();
 			$('#podstable tr[pod_name="' + podName + '"] td div[column=status] span').text('Deleting');
 		},
-		error: function(xhr) {
-			$('#podstable tr[pod_name="' + podName + '"] td a.delete-pod').show();
-			$('#podstable tr[pod_name="' + podName + '"] td div[column=status] span').text('Delete failed');
-		},
 		complete: function(xhr) {
 			ajaxCompleted(xhr);
 		},
@@ -181,6 +181,8 @@ function deletePod(podName) {
 					OC.redirect('/');
 				} else {
 					OC.dialogs.alert(t("user_pods", "delete_pod: Something went wrong..."), t("user_pods", "Error"));
+					$('#podstable tr[pod_name="' + podName + '"] td a.delete-pod').show();
+					$('#podstable tr[pod_name="' + podName + '"] td div[column=status] span').text('Delete failed');
 				}
 			}
 		}
